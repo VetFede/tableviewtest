93a765f505ef2c31a10dff5ddde5c82572a8c350
federico spagocci <veterinario@libero.it>
2018-11-03 09:21:28 +0100

    Initial Commit

---
+++ test_tableview2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test_tableview2
+//
+//  Created by federico SPAGOCCI on 03/11/2018.
+//  Copyright © 2018 Inoob_Spagocci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
